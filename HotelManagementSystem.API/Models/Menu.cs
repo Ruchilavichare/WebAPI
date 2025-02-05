3d60e3f53a1e6a9ddff31f91cce8927ad50302d4
ruchilavichare <ruchila.vichare@gmail.com>
2025-02-05 14:42:22 +0530

    WEB API example

---
+++ HotelManagementSystem.API/Models/Menu.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelManagementSystem.API.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
