070be6a9a8b7d85c2abde63a8a9813966d4022c1
ruchilavichare <ruchila.vichare@gmail.com>
2025-02-20 01:21:19 +0530

    consuming Toy API that will show "Best for" Section, "Deals of the Day", High-Converting Toys etc

---
+++ ToyMVC/Controllers/ToyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ToyMVC.Models;
+using ToyMVC.Services;
+
+namespace ToyMVC.Controllers
+{
+    public class ToyController : Controller
+    {
+        private readonly ToyService _toyService;
+
+        public ToyController(ToyService toyService)
+        {
+            _toyService = toyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var toys = await _toyService.GetHighConvertingToysAsync();
+            return View(toys);
+        }
+
+        public async Task<IActionResult> TopRated()
+        {
+            var toys = await _toyService.GetTopRatedToysAsync();
+            return View(toys);
+        }
+
+        public async Task<IActionResult> BestForCategory(string category)
+        {
+            var toys = await _toyService.GetToysByCategoryAsync(category);
+            return View(toys);
+        }
+
+        public async Task<IActionResult> DealsOfTheDay()
+        {
+            var toys = await _toyService.GetDealsOfTheDayAsync();
+            return View(toys);
+        }
+    }
+}
---
+++ ToyMVC/Models/Toy.cs
@@ -0,0 +1,17 @@
+﻿namespace ToyMVC.Models
+{
+    public class Toy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        public int ReviewsCount { get; set; }
+        public bool IsBestSeller { get; set; }
+        public DateTime AddedDate { get; set; }
+        public string Category { get; set; }
+        public string AffiliateLink { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool IsDealOfTheDay { get; set; }
+    }
+}
